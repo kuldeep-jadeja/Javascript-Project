5a7a67cc3309dadea3e81d15f855ebea0911e124
kuldeep-jadeja <kuldeepjadeja7414@gmail.com>
2024-07-23 14:23:22 +0530

    Color Changer

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -4,15 +4,6 @@ const body = document.querySelector('body');
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
     e.target;
-    // if (e.target.id === 'grey') {
-    //   body.style.backgroundColor = 'grey';
-    // } else if (e.target.id === 'white') {
-    //   body.style.backgroundColor = 'white';
-    // } else if (e.target.id === 'blue') {
-    //   body.style.backgroundColor = 'blue';
-    // } else if (e.target.id === 'yellow') {
-    //   body.style.backgroundColor = 'yellow';
-    // }
     switch (e.target) {
       case grey:
         body.style.backgroundColor = e.target.id;
